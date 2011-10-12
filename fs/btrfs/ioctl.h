9f473dd4fd5521c3486a03b8c5c9b36b94fe237c
Ilya Dryomov <idryomov@gmail.com>
2011-10-12 10:09:43 +0300

    Btrfs: allow for pausing restriper
    Implement an ioctl for pausing restriper.  This pauses the relocation,
    but restripe is still considered to be "in progress": restriper item is
    not deleted, other volume operations cannot be started, etc.  If paused
    in the middle of profile changing operation we will continue making
    allocations with the target profile.
    
    Add a hook to close_ctree() to be able to pause restriper and free it's
    data structures on unmount.  (It's safe to unmount when restriper is in
    'paused' state, we will resume with the same parameters on the next
    mount)
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/btrfs/ioctl.h
+++ fs/btrfs/ioctl.h
@@ -110,6 +110,7 @@ struct btrfs_ioctl_fs_info_args {
 };
 
 #define BTRFS_RESTRIPE_CTL_CANCEL	1
+#define BTRFS_RESTRIPE_CTL_PAUSE	2
 
 /*
  * this is packed, because it should have the same size as its
