4d988ed3c7f4debde55ec6f58ac92cb977ff722b
Ilya Dryomov <idryomov@gmail.com>
2011-10-12 10:09:42 +0300

    Btrfs: recover restripe on mount
    On mount, if restripe item is found, resume restripe in a separate
    kernel thread.
    
    Try to be smart to continue roughly where previous balance (or convert)
    was interrupted.  For chunk types that were being converted to some
    profile we turn on soft convert, in case of a simple balance we turn on
    usage filter and relocate only less-than-90%-full chunks of that type.
    These are just heuristics but they help quite a bit, and can be improved
    in future.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -2419,6 +2419,10 @@ retry_root_backup:
 		if (!err)
 			err = btrfs_orphan_cleanup(fs_info->tree_root);
 		up_read(&fs_info->cleanup_work_sem);
+
+		if (!err)
+			err = btrfs_recover_restripe(fs_info->tree_root);
+
 		if (err) {
 			close_ctree(tree_root);
 			return ERR_PTR(err);
