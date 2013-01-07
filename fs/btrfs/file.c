8a9eb111f375731a857024ee424eaee823a1196a
Liu Bo <bo.li.liu@oracle.com>
2013-01-07 10:10:12 +0000

    Btrfs: fix a bug when llseek for delalloc bytes behind prealloc extents
    xfstests case 285 complains.
    
    It it because btrfs did not try to find unwritten delalloc
    bytes(only dirty pages, not yet writeback) behind prealloc
    extents, it ends up finding nothing while we're with SEEK_DATA.
    
    Signed-off-by: Liu Bo <bo.li.liu@oracle.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -2329,9 +2329,12 @@ static int find_desired_extent(struct inode *inode, loff_t *offset, int origin)
 					}
 				}
 
-				*offset = start;
-				free_extent_map(em);
-				break;
+				if (!test_bit(EXTENT_FLAG_PREALLOC,
+					      &em->flags)) {
+					*offset = start;
+					free_extent_map(em);
+					break;
+				}
 			}
 		}
 
